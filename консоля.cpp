2a084bf2b65476e6af83dedb586dbb5c01260896
Kika7536 <104264433+Kika7536@users.noreply.github.com>
2024-09-19 23:20:23 +0300

    Добавьте файлы проекта.

---
+++ консоля.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+
+
+void main() {
+	setlocale(LC_ALL, "rus");
+
+	int hour, i = 0;
+
+	cout << "Задание 1. Кукушка" << endl;
+	cout << "Условие:" << endl;
+	cout << "Прграмма работает при том условии, что время будет вводиться в 24-х часовом формате," << endl
+		<< "где счет начинается с 1, а 24 является полночью (эквивалентом 00:00)" << endl << endl;
+
+	
+
+	
+		cout << "Введите, который час: ";
+		cin >> hour;
+
+		if (hour < 1 || hour > 24) {
+			cout << "Ошибка ввода. Попробуйте еще раз." << endl << endl;
+			cout << "-----------------------" << endl << endl;
+		}
+		else {
+			while (i < hour) {
+				cout << "Ку-ку" << endl;
+				++i;
+
+			}
+		}
+		i = 0;
+	
+}
